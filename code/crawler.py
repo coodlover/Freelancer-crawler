be70c5e12bbf17e38694e0fbd7980a3e6b43696a
coodlover <1594487572@qq.com>
2020-07-08 15:11:12 +0800

    add proxy and header

--- code/crawler.py
+++ code/crawler.py
@@ -1,17 +1,66 @@
 from bs4 import BeautifulSoup
 import requests
-import time
 import pymysql
+import random
+my_headers = [
+    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14",
+    "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)",
+    'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11',
+    'Opera/9.25 (Windows NT 5.1; U; en)',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+    'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)',
+    'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
+    'Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9',
+    "Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Ubuntu/11.04 Chromium/16.0.912.77 Chrome/16.0.912.77 Safari/535.7",
+    "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0 "
+]
+
+proxy_list = [
+    '183.95.80.102:8080',
+    '123.160.31.71:8080',
+    '115.231.128.79:8080',
+    '166.111.77.32:80',
+    '43.240.138.31:8080',
+    '218.201.98.196:3128'
+]
+
+
+def getheader():
+    return {'User-Agent': random.choice(my_headers)}
+
+
+def getproxy():
+    return {'http': 'http://'+random.choice(proxy_list)}
+
+PROXY_POOL_URL = 'http://localhost:5555/random'
+
+
+def get_proxy():
+ try:
+    response = requests.get(PROXY_POOL_URL)
+    if response.status_code == 200:
+        return response.text
+ except ConnectionError:
+    return None
+
 
 def getHTML(url):
+    proxy = getproxy()
+    header = getheader()
+    print(proxy)
+    print(header)
     try:
-        r=requests.get(url,timeout=200)
+        r = requests.get(url, headers=header, proxies=proxy, timeout=200)
         r.raise_for_status()
-        r.encoding=r.apparent_encoding
+        r.encoding = r.apparent_encoding
         return r.text
     except:
         print("获取Url失败:", url)
         return 0
+
 
 def getProUrl(html, proUrl):
     if html == 0:
@@ -23,12 +72,13 @@ def getProUrl(html, proUrl):
         if project == '\n':
             continue
         status = project.find(class_='JobSearchCard-primary-heading-days').string
-        if status != "已经结束 left":
-            continue
+        # if status != "已经结束 left":
+        #     continue
         url = project.find(class_='JobSearchCard-primary-heading-link')['href']
         proUrlList.append(url)
     print("******", proUrl, "本页共有", len(proUrlList), "个项目")
     return proUrlList
+
 
 def getDetail(html, url):
     if html == 0:
@@ -70,6 +120,7 @@ def getDetail(html, url):
         detailDic['devList'].append(devDic)
     print(" 共有", len(detailDic['devList']), "个开发者", end=" ")
     return detailDic
+
 
 def sqlExe(db, cursor, sql, params):
     try:
@@ -115,13 +166,28 @@ def mysqlWrite(detailDic, db, cursor):
     sqlExe(db, cursor, sql, params)
     print("写入完成")
 
+
+# def writelog(page, num):
+#     file = open("crawler.log", 'w', encoding="utf-8")
+#     file.write(page)
+#     file.write(num)
+#     file.close()
+#
+# def readlog():
+#     file = open("crawler.log", 'r')
+#     page = file.readline().strip()
+#     num = file.readline().strip()
+#     return page, num
+
 def control():
-    db = pymysql.connect("localhost", "root", "19971002", "freelancer")
+    db = pymysql.connect("localhost", "root", "1234", "freelancer")
     cursor = db.cursor()
-    for i in range(39, 151):
+    for i in range(58, 151):
         proUrl = "https://www.freelancer.cn/jobs/" + str(i+1) + "/?status=all&languages=zh"
         proUrlList = getProUrl(getHTML(proUrl), proUrl)
         n = 1
+        if proUrlList == 0:
+            return
         total = len(proUrlList)
         for url in proUrlList:
             print("{}/{} ".format(n, total), end=" ")
@@ -130,8 +196,9 @@ def control():
             if not detailDic:
                 continue
             mysqlWrite(detailDic, db, cursor)
-            time.sleep(3)  # 防止系统检测封IP
+            #time.sleep(3)  # 防止系统检测封IP
     db.close()
+
 
 if __name__ == "__main__":
     control()
